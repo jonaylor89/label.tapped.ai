9bf60e71c44774eb69611b067589da8cb22f6913
Johannes Naylor <jonaylor89@gmail.com>
2023-07-30 13:24:55 +0200

    in a spot

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      tapped: '#3A84C6',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'manrope': 'Manrope',
       },
     },
   },
